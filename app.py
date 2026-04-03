26584c5d2f43d624fc6b039e5da8fbc6a176630a
keshavmurthyhg <keshavait@gmail.com>
2026-04-03 17:30:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ st.title("📊 Unified Dashboard (SNOW / Azure / PTC)")
 @st.cache_data
 def load_data():
     # 🔁 Replace with your OneDrive links later
-    azure = pd.read_excel("https://YOUR_LINK_AZURE.xlsx")
-    snow = pd.read_excel("https://YOUR_LINK_SNOW.xlsx")
-    ptc = pd.read_excel("https://YOUR_LINK_PTC.xlsx")
+    azure = pd.read_excel("https://volvogroup-my.sharepoint.com/:x:/r/personal/keshavamurthy_hg_consultant_volvo_com/Documents/Keshava/AMS-Windchill/Keshava/Windchill%20Support%20Documents/Azure%20Bugs/Bug%20Report/All%20VCE%20Bugs/All%20VCE%20Bugs_31Mar2026.csv?d=wf77ad2caf2d64196a8517f3c36b1b83e&csf=1&web=1&e=leg0qx")
+    ptc = pd.read_excel("https://volvogroup-my.sharepoint.com/:x:/r/personal/keshavamurthy_hg_consultant_volvo_com/Documents/Keshava/AMS-Windchill/Keshava/Windchill%20Support%20Documents/PTC_Cases_Report/PTC%20Report/PTC%20Cases%20Report_30Mar2026.xlsx?d=w9b941604acc94594957f50bfc4bc55a7&csf=1&web=1&e=y259KV")
+    snow = pd.read_excel("https://volvogroup-my.sharepoint.com/:x:/r/personal/keshavamurthy_hg_consultant_volvo_com/Documents/Keshava/AMS-Windchill/Keshava/Windchill%20Support%20Documents/VCE-Incidents_Report/Reports/incident_31Mar2026.xlsx?d=w4b1ad8a4e5314c98ae5ef5a767693646&csf=1&web=1&e=gz9MyI")
 
     azure["Source"] = "Azure"
     snow["Source"] = "SNOW"
